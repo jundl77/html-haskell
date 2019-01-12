43f041f6bd093d32019743ee751439f1cd7d2154
jundl77 <julianbrendl@gmail.com>
2019-01-12 22:58:42 +0100

    fixed cors

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,8 @@ import           Web.Spock
 import           Web.Spock.Config
 import           Data.Aeson       hiding (json)
 import           Data.Text        (Text, pack)
+import           Network.Wai                      
+import           Network.Wai.Middleware.AddHeaders
 import           Network.Wai.Middleware.Cors
 import           Network.Wai.Middleware.RequestLogger
 
@@ -23,10 +25,26 @@ data PostData = PostData { code :: Text } deriving (Generic, Show)
 instance ToJSON PostData
 instance FromJSON PostData
 
+-- | CORS middleware configured with 'appCorsResourcePolicy'.
+corsified :: Middleware
+corsified = cors (const $ Just appCorsResourcePolicy)
+
+appCorsResourcePolicy :: CorsResourcePolicy
+appCorsResourcePolicy = CorsResourcePolicy {
+    corsOrigins        = Nothing
+  , corsMethods        = ["OPTIONS", "GET", "PUT", "POST"]
+  , corsRequestHeaders = ["Authorization", "Content-Type"]
+  , corsExposedHeaders = Nothing
+  , corsMaxAge         = Nothing
+  , corsVaryOrigin     = False
+  , corsRequireOrigin  = False
+  , corsIgnoreFailures = False
+}
+
 main :: IO ()
 main = do
   let port = 3001
-  let middlewares = fmap ((simpleCors . logStdoutDev) .)
+  let middlewares = fmap ((corsified . logStdoutDev) .)
   spockCfg <- defaultSpockCfg () PCNoDatabase ()
   runSpock port $ middlewares $ (spock spockCfg app)
 
